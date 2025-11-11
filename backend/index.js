05d23ab86ae227d28d1d934bd2020bd06b76b3fd
tanmaypaliwal576 <tanmayprofessional576@gmail.com>
2025-11-12 01:14:46 +0530

    index updated

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("/.*/", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
