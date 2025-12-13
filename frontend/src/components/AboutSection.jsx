a3a14226a4ef28a2cc394a99e1c3446d0e9ecf66
tanmaypaliwal576 <tanmayprofessional576@gmail.com>
2025-12-13 17:16:25 +0530

    Done

--- frontend/src/components/AboutSection.jsx
+++ frontend/src/components/AboutSection.jsx
@@ -114,11 +114,11 @@ export default function AboutMe() {
 
           {/* Download CV Button */}
           <a
-            href="/Resume/TP_Resume_1.pdf"
+            href="/Resume/TP_Resume_WP.pdf"
             download="Tanmay_Paliwal_Resume.pdf"
             className="inline-block border border-purple-500 px-6 py-2 rounded-full text-white hover:bg-purple-600/20 transition"
           >
-            Download CV
+            Download Resume
           </a>
         </div>
       </div>
